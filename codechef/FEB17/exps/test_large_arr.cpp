208290d8d91a09dd5f0def5acd62a2616e8bf791
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-02-15 00:14:18 +0530

    codechef/FEB17/exps/test_large_arr.cpp

---
+++ codechef/FEB17/exps/test_large_arr.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    //bool a[1000000000000000000];
+    //long long l;
+
+    //l = pow(10, 18);
+    //cout << l;
+
+    //memset(a, 0, sizeof(a));
+
+    //for (int i = 0; i < sizeof(a); i++) cout << a[i];
+
+    cout << log2l(1000000000000000000) << endl << pow(2, 59);
+
+}
