e9ef3c785496d83085510e84de27fb7c7256db96
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-03-26 23:55:03 +0530

    codejam/prac/2016/qual/A/A.cpp

---
+++ codejam/prac/2016/qual/A/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	ifstream cin ("A-small-practice.in");
+	//ofstream cout ("A-large-practice.out");
+
+	int t, n, c;
+	cin >> t;
+	for(int i = 1; i <= t; i++) {
+		cin >> n;
+		c = 0;
+		cout << "CASE #" << i << "\n";
+	}
+
+	return 0;
+}
