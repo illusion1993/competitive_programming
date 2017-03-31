7dee5a38292138d5244dfbfd751f1d6233c90f2b
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-03-31 17:03:07 +0530

    codeforces/April_Fools_2016/A.cpp

---
+++ codeforces/April_Fools_2016/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int p;
+	cin >> p;
+	cout << pow(2, p);
+}
