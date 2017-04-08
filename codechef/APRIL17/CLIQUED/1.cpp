2fab45c0a61b210ade2b8d4fdee528b148829305
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-04-08 10:41:10 +0530

    codechef/APRIL17/CLIQUED/1.cpp

---
+++ codechef/APRIL17/CLIQUED/1.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+ll min_dist[100000];
+int n, k, x, m, s;
+
+map<int, vector<pair<int, int> > > paths;   // node: < <dist, node> , ... >
+
+void traverse(int node, ll dist_so_far, int parent) {
+    min_dist[node] = dist_so_far;
+    
+    bool not_old = (node >= k);
+    if (paths.count(node)) {
+        sort(paths[node].begin(), paths[node].end());
+        int i;
+        for (i = 0; i < paths[node].size() && (not_old || paths[node][i].first < x); i++) {
+            if (paths[node][i].second != parent) {
+                if (dist_so_far + (ll)paths[node][i].first < min_dist[paths[node][i].second]) 
+                    traverse(paths[node][i].second, dist_so_far + (ll)paths[node][i].first, node);
+            }
+        }
+        if (!not_old) {
+            for (int j = 0; j < k; j++) {
+                if (j != node) {
+                    if (min_dist[j] > dist_so_far + x) {
+                        min_dist[j] = (ll)(dist_so_far + x);
+                        traverse(j, min_dist[j], node);
+                    }
+                }
+            }
+            while (i < paths[node].size()) {
+                if (paths[node][i].second != parent) {
+                    if (dist_so_far + (ll)paths[node][i].first < min_dist[paths[node][i].second]) 
+                        traverse(paths[node][i].second, dist_so_far + (ll)paths[node][i].first, node);
+                }
+                i++;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int t;
+    cin >> t;
+    while(t--) {
+        cin >> n >> k >> x >> m >> s;
+        paths.clear();
+        for (int i = 0; i < n; i++) min_dist[i] = INT_MAX;
+        // min_dist[s - 1] = 0;
+
+        int tmp1, tmp2, tmp3;
+        for (int i = 0; i < m; i++) {
+            cin >> tmp1 >> tmp2 >> tmp3;
+            if (paths.count(tmp1 - 1)) paths[tmp1 - 1].push_back({tmp3, tmp2 - 1});
+            else {
+                vector<pair<int, int> > p;
+                p.push_back({tmp3, tmp2 - 1});
+                paths[tmp1 - 1] = p;
+            }
+            if (paths.count(tmp2 - 1)) paths[tmp2 - 1].push_back({tmp3, tmp1 - 1});
+            else {
+                vector<pair<int, int> > p;
+                p.push_back({tmp3, tmp1 - 1});
+                paths[tmp2 - 1] = p;
+            }
+        }
+        traverse(s - 1, 0, -1);
+        for (int i = 0; i < n; i++) cout << min_dist[i] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
