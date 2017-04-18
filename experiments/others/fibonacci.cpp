f2f4a88bffc7a5f865c48da7060c2cb1108402a0
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-04-19 04:25:07 +0530

    experiments/others/fibonacci.cpp

---
+++ experiments/others/fibonacci.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll unsigned long long
+#define MAXRANGE 40000000000000001
+
+vector<ll> f;
+void findEvenFibs() {
+	ll a = 1, b = 1, tmp;
+	while (b <= MAXRANGE) {
+		if (b % 2 == 0) f.push_back(b);
+		tmp = b;
+		b += a;
+		a = tmp;
+	}
+}
+
+int main(){
+    findEvenFibs();
+    int t, pos;
+    ll s, n;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        s = 0;
+        cin >> n;
+        pos = 0;
+        while(f[pos] <= n) {
+            s += f[pos];
+            pos++;
+        }
+        cout << s << endl;
+    }
+    return 0;
+}
