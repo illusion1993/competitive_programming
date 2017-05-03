6ba157ea08f08e7ffa12eca1bc6c8f030e428f05
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2017-05-03 15:57:34 +0530

    codechef/PRAC/ORDERS_opt.cpp

---
+++ codechef/PRAC/ORDERS_opt.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    double m = DBL_MAX;
+    for (int i = 0; i < 1000; i++) {
+    	cout << i + 1 << "\t" << m << "\n";
+    	m /= 2;
+    }
+    return 0;
+
+    vector<pair<double, int> > rank_index;
+
+	int t, tmp;
+	cin >> t;
+	while(t--) {
+		rank_index.clear();
+	}
+
+	return 0;
+}
