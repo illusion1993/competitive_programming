2ecb87c985ed3c62c89b01cef40b96a5b2fbda0e
illusion1993 <rathore.vikram624@gmail.com>
2017-12-03 21:22:03 +0530

    Auto-commit #21

---
+++ hackerearth/zscaler/tc.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	srand(0);
+	int n = 10, q = 5, range = 10;
+	int arr[n];
+
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 
+	}
+
+	return 0;
+}
